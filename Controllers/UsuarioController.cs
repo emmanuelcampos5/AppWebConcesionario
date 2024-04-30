14ec07e48e01ae62e6cf868bf373cb269a7c2082
emmanuelcampos5 <emmanuelcampos2111@gmail.com>
2024-04-29 23:09:28 -0600

    Initial commit

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,19 @@
+﻿using AppWebConcesionario.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppWebConcesionario.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public UsuarioController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Listado()
+        {
+            return View(_context.Usuario.ToList());
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppWebConcesionario.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int idUsuario { get; set; }
+        public int idRol { get; set; }
+        public string nombreUsuario { get; set; }
+        public string cedulaUsuario { get; set; }
+        public string telefonoUsuario { get; set; }
+        public string correoUsuario { get; set; }
+        public string lugarResidencia { get; set; }
+        public string password { get; set; }
+        public bool restablecer { get; set; }
+        public bool estadoActivo { get; set; }
+        public bool estadoSuscripcion { get; set; }
+    }
+}

72defda297786dc0d4482203425298bb301d9feb
khris-47 <rodriguezchris808@gmail.com>
2024-05-08 20:35:34 -0600

    ..

--- Models/Factura.cs
+++ Models/Factura.cs
@@ -5,13 +5,23 @@ namespace AppWebConcesionario.Models
     public class Factura
     {
         [Key]
+        [Required(ErrorMessage = "Debe ingresar el id de la factura")]
         public int idFactura { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar el id del usuario")]
         public int idUsuario { get; set; }
+
+
         public double subtotal { get; set; }
         public double montoDescuento { get; set; }
         public double montoImpuesto { get; set; }
         public double totalFactura { get; set; }
+
+        [DataType(DataType.DateTime)]
         public DateTime fechaFactura { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar el id del usuario")]
+        [DataType(DataType.Text)]
         public string tipoPago { get; set; }
     }
 }

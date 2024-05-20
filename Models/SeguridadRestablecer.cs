f33251f78b72ed1035dfbf46e0ce5e21fc1f19a6
emmanuelcampos5 <emmanuelcampos2111@gmail.com>
2024-05-19 21:32:05 -0600

    modulo usuarios casi completo

---
+++ Models/SeguridadRestablecer.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppWebConcesionario.Models
+{
+    public class SeguridadRestablecer
+    {
+        public string correoUsuario { get; set; }
+
+        [Required(ErrorMessage = "Digite la contraseña enviada por email")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+
+
+        [Required(ErrorMessage = "Digite la nueva contraseña")]
+        [DataType(DataType.Password)]
+        public string newPassword { get; set; }
+
+
+        [Required(ErrorMessage = "Confirme la contraseña")]
+        [DataType(DataType.Password)]
+        public string confirmar { get; set; }
+
+    }
+}
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace AppWebConcesionario.Models
@@ -8,10 +9,26 @@ namespace AppWebConcesionario.Models
         [Key]
         public int idUsuario { get; set; }
         public int idRol { get; set; }
+
+        [Required]
+        [DisplayName("Nombre de Usuario")]
         public string nombreUsuario { get; set; }
+
+        [Required]
+        [DisplayName("Cedula")]
         public string cedulaUsuario { get; set; }
+
+        [Required]
+        [DisplayName("Telefono")]
         public string telefonoUsuario { get; set; }
+
+        [Required]
+        [DisplayName("Correo Electronico")]
+        [DataType(DataType.EmailAddress)]
         public string correoUsuario { get; set; }
+
+        [Required(ErrorMessage = "El campo Lugar de Residencia es obligatorio.")]
+        [DisplayName("Lugar de Residencia")]
         public string lugarResidencia { get; set; }
         public string password { get; set; }
         public bool restablecer { get; set; }
